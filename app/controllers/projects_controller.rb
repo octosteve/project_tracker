847083ffdafc74c774ed21424cb58c98e5bf10a3
Steven Nunez <steven.nunez@gmail.com>
2016-01-27 10:54:00 -0500

    Create new project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,24 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :github_url)
   end
 end
